c5d9c3de40cfae461ffd3e04c93ae7e7e47f40ff
Axel Somerseth Córdova López <axelsomerseth@gmail.com>
2022-07-12 22:56:46 -0600

    init typescript

---
+++ source/index.ts
@@ -0,0 +1,12 @@
+// any type
+const greeting: any = "Hello world";
+
+// Built-in types
+const five: number = 5;
+const firstName: string = "Axel";
+const isTypeScript: boolean = true;
+let voidVar: void;
+let nullVar: null;
+let undefinedVar: undefined;
+
+// User-defined types
